383135293c6821c4ef7957d772c9b09cf3dc294c
fluxxxto <141028343+fluxxxto@users.noreply.github.com>
2024-01-09 22:01:48 +0100

    Update index.js

--- gmazon/index.js
+++ gmazon/index.js
@@ -381,6 +381,7 @@ function showDeliveryPopup() {
                 <option value="person">Liefern zu Person</option>
                 <option value="kiste">Liefern zu Kiste/Briefkasten</option>
             </select>
+            <p>KAU IMMER angeben. (bei überweisung von firmenkonto auch name)</p>
             <p id="delivery-info"></p>
             <input type="text" id="delivery-location" placeholder="Kau/Ort">
             <label for="payment-method">Zahlungsmethode:</label>
